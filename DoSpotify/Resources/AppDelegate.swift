14190262a69539cec5360260c7997b690b2cecc9
Adonis <adonis.rumbwere@cassavasmartech.co.zw>
2021-02-25 17:52:30 +0200

    added Auth Manager to remember signed in User

--- DoSpotify/Resources/AppDelegate.swift
+++ DoSpotify/Resources/AppDelegate.swift
@@ -10,11 +10,24 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        if AuthManager.shared.isSignedIn {
+            window.rootViewController = TabBarViewController()
+        } else {
+            let NavVC = UINavigationController(rootViewController: WelcomeViewController())
+            NavVC.navigationBar.prefersLargeTitles = true
+            NavVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
+            window.rootViewController = NavVC
+        }
+        
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
